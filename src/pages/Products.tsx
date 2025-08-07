fd7a79aa5e5dd40080e64b573c5d30840e76a84d
younus358cc <ayounus358cc@gmail.com>
2025-08-07 09:19:53 +0600

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -152,7 +152,7 @@ const Products: React.FC = () => {
         category: 'agricultural',
         model: 'SM-16L',
         brand: 'এগ্রো টুলস',
-        image: 'https://www.gardentoolsbd.com/storage/12l-spray.png',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPWYzHIpFSFN_mUgVWV_Ubc1Id1AaUifhlwg&s',
         price: '৳ ৩,৫০০ - ৳ ৫,০০০'
       },
       {
